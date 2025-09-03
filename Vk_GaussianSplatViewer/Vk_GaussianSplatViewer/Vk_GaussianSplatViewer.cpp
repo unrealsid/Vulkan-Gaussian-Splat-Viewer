f545cf858f882a297d9c8cd5cead785feba1a704
Sid <7568554+unrealsid@users.noreply.github.com>
2025-09-03 10:09:27 +0530

    Initial Submit

---
+++ Vk_GaussianSplatViewer/Vk_GaussianSplatViewer/Vk_GaussianSplatViewer.cpp
@@ -0,0 +1,13 @@
+﻿// Vk_GaussianSplatViewer.cpp : Defines the entry point for the application.
+//
+
+#include "Vk_GaussianSplatViewer.h"
+#define VK_ENABLE_BETA_EXTENSIONS
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
